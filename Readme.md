5386f8a473cf8f7b97c0fa8116f2801013b4cffe
宇天 <yutent@doui.cc>
2018-05-24 19:53:23 +0800

    优化get方法;headers重命名为header;

--- Readme.md
+++ Readme.md
@@ -2,57 +2,89 @@
 
 # http.request
 
-> `http.request` is a module that let you can easily using on http server. 
-
-
+> `http.request` is a module that let you can easily using on http server.
 
 ## Install
+
 ```bash
-    npm install http.request
+    npm i http.request
 ```
-
-
 
 ## Usage
+
 ```javascript
+let Request = require('http.request')
+let http = require('http')
 
-let Request = require('http.request'),
-    http = require('http');
+http
+  .createServer((req, res) => {
+    let request = new Request(req, res)
 
+    console.log(request.origin) // {req, res}
 
-    http.createServer((req, res) => {
+    // print the fixed url
+    console.log(request.url)
 
-    
-        let request = new Request(req, res);
+    request.ip() // get client ip address
 
-        // it eq. argument req
-        console.log(request.req)
-        
-        // print the fixed url
-        console.log(request.url)
-        
-        request.ip() // get client ip address
-        
-        // http://test.com/?foo=bar
-        request.get('foo') // bar
-
-
-    }).listen(3000)
-
-
+    // http://test.com/?foo=bar
+    request.get('foo') // bar
+  })
+  .listen(3000)
 ```
-
-
-
 
 ## API
 
-### get([key[,xss]])
-- key `<String>` optional
-- xss `<Boolean>` optional
+### origin 
+> return the origin request object and response object.
 
-> Get the fieldset from url. Just like PHP's $_GET[];
-> 
+```js
+console.log(request.origin) // {req: request, res: response}
+```
+
+
+### app
+> return this first part of url
+
+```js
+// abc.com/foo/bar
+console.log(request.app) // foo
+```
+
+
+### path
+> return this extra part of url
+
+```js
+// abc.com/foo/bar/aa/bb
+console.log(request.path) // ['bar', 'aa', 'bb']
+```
+
+### url
+> return this fixed url
+
+```js
+// abc.com/foo/bar/aa/bb
+// abc.com////foo///bar/aa/bb
+console.log(request.url) // foo/bar/aa/bb
+```
+
+### router
+> return this router params
+
+```js
+// abc.com/foo/bar/aa/bb/xx/yy
+console.log(request.router) // {aa: 'bb', xx: 'yy'}
+```
+
+
+
+### get([key[,xss]])
+
+* key `<String>` optional
+* xss `<Boolean>` optional
+
+> Get the fieldset from url. Just like PHP's `$_GET[]`;
 > If `xss` is set to be true, the result will be filtered out with base xss.
 
 ```javascript
@@ -61,18 +93,17 @@ request.get('name') // foo
 request.get('age') // 18
 
 // return all if not yet argument given
-request.get() // {name: 'foo', age: 18} 
+request.get() // {name: 'foo', age: 18}
 request.get('weight') // return null if not exists
 ```
 
-
-
 ### post([key[,xss]])
-- key `<String>` optional
-- xss `<Boolean>` optional
 
-> Get the http body content, just like PHP's $_POST[].
-> 
+* key `<String>` optional
+* xss `<Boolean>` optional
+
+> Get the http body content, just like PHP's `$_POST[]`.
+>
 > **this function must use await/yiled command**
 
 ```javascript
@@ -84,25 +115,22 @@ await request.post('age') // 18
 await request.post() // {name: 'foo', age: 18}
 await request.post('weight') // return null if not exists
 ```
-    
 
-### headers([key])
-- key `<String>` optional
+### header([key])
+
+* key `<String>` optional
 
 > return http headers.
 
 ```javascript
-request.headers('user-agent') // Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ...
+request.header('user-agent') // Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ...
 
 // return all if not yet argument given
-request.headers() // {'user-agent': '...'[, ...]}
+request.header() // {'user-agent': '...'[, ...]}
 ```
-    
-    
-    
+
 ### ip()
+
 > return the client IP address.
-> 
+>
 > It would return '127.0.0.1' maybe if in local area network.
-
-
--- index.js
+++ index.js
@@ -1,251 +1,293 @@
 /**
- * 
+ *
  * @authors yutent (yutent@doui.cc)
  * @date    2017-03-16 18:27:29
  *
  */
 
-"use strict";
-const Parser = require('./lib'),
-    FS = require('iofs'),
-    URL = require('url'),
-    QS = require('querystring'),
-    tmpdir = __dirname + '/.tmp/';
+'use strict'
+const Parser = require('./lib')
+const FS = require('iofs')
+const URL = require('url')
+const QS = require('querystring')
+const tmpdir = __dirname + '/.tmp/'
 
-
-class Request {
-
-    constructor(req, res){
-        this.req = req
-        this.res = res
-        this._getParam = null
-        this._postParam = null
-        this.method = req.method.toUpperCase()
-        this.router = {}
-        this._fixUrl()
-
-        //把判断放到
-        if(!FS.isdir(tmpdir)){
-            FS.mkdir(tmpdir)
-        }else{
-            //清除2个小时前的所有临时文件
-            let list = FS.ls(tmpdir)
-            list.forEach(it => {
-                if(FS.stat(tmpdir + it).atime < Date.now() - 7200000)
-                    FS.rm(tmpdir + it)
-            })
-        }
-    }
-
-    //修正请求的url
-    _fixUrl(){
-        let _url = URL.parse(this.req.url).pathname.slice(1).replace(/[\/]+$/, ''),
-            _route = '',
-            pathArr = [],
-            _routeArr = [];
-
-
-        if(/[^\w\-\/\.]/.test(_url)){
-            this.res.rendered = true
-            this.res.writeHead(400, {'X-debug': 'url[' + _url + '] contains illegal characters'})
-            return this.res.end('')
-        }
-        
-        //修正url中可能出现的"多斜杠",并跳转到修正后的地址
-        _url = _url.replace(/[\/]+/g, '/').replace(/^\//, '');
-
-        pathArr = _url.split('/')
-        if(!pathArr[0] || pathArr[0] === '')
-            pathArr[0] = 'index'
-
-        if(pathArr[0].indexOf('.') !== -1){
-            _route = pathArr[0].slice(0, pathArr[0].indexOf('.'))
-            //如果_route为空(这种情况一般是url前面带了个"."造成的),则自动默认为index
-            if(!_route || _route === '')
-                _route = 'index'
-        }else{
-            _route = pathArr[0]
-        }
-
-        pathArr.shift()
-
-        _routeArr = Array.from(pathArr.slice(1))
-        while(_routeArr.length){
-            this.router[_routeArr.shift()] = _routeArr.shift() || null
-        }
-        _routeArr = undefined
-
-        for(let i in this.router){
-            if(!this.router[i]){
-                continue
-            }
-
-            if(this.router[i].startsWith(0) && !this.router[i].startsWith('0.'))
-                continue
-            else if(isFinite(this.router[i]))
-                this.router[i] = +this.router[i]
-        }
-
-        this.url = _url
-        this.path = pathArr
-        this.app = _route
-    }
-
-    /**
-     * [get 同php的$_GET]
-     */
-    get(key = '', xss = true){
-        xss = !!xss
-        if(!this._getParam){
-            let para = URL.parse(this.req.url).query
-            para = Object.assign({}, QS.parse(para))
-            if(xss){
-                for(let i in para){
-                    para[i] = para[i].trim().xss();
-
-                    if(!para[i]){
-                        continue
-                    }
-
-                    if(para[i].startsWith(0) && !para[i].startsWith('0.'))
-                        continue
-                    else if(isFinite(para[i]))
-                        para[i] = +para[i]
-                }
-            }
-            this._getParam = para
-        }
-        
-        return key ? (this._getParam.hasOwnProperty(key) ? this._getParam[key] : null) : this._getParam
-    }
-
-    /**
-     * [post 接收post, 需要 yield ]
-     * @param  {Str}    key      [字段]
-     */
-    post(key = '', xss = true){
-
-        let para = {}
-        xss = !!xss
-
-        //如果之前已经缓存过,则直接从缓存读取
-        if(this._postParam){
-            if(key)
-                return this._postParam.hasOwnProperty(key) ? this._postParam[key] : null
-            else
-                return this._postParam
-        }
-
-        return new Promise((yes, no) => {
-            let form = new Parser()
-            form.uploadDir = tmpdir
-            form.parse(this.req)
-
-            form.on('field', (name, value) => {
-                if(/urlencoded/i.test(this.headers('content-type'))){
-                    if(name.slice(0, 2) === '{"' && (name.slice(-2) === '"}' || value.slice(-2) === '"}')){
-
-                        name = name.replace(/\s/g, '+')
-
-                        if(value.slice(0, 1) === '=')
-                            value = '=' + value
-
-                        return Object.assign(para, JSON.parse(name + value))
-                    }
-                }
-
-                if(typeof value === 'string')
-                    value = xss ? value.xss() : value
-
-                if(name.slice(-2) === '[]'){
-                    name = name.slice(0, -2)
-                    if(typeof value === 'string')
-                        value = [value]
-
-                }else if(name.slice(-1) === ']'){
-                    let key = name.slice(name.lastIndexOf('[') + 1, -1)
-                    name = name.slice(0, name.lastIndexOf('['))
-
-                    //多解析一层对象(也仅支持到这一层)
-                    if(name.slice(-1) === ']'){
-                        let pkey = name.slice(name.lastIndexOf('[') + 1, -1)
-                        name = name.slice(0, name.lastIndexOf('[')) 
-
-                        if(!para.hasOwnProperty(name))
-                            para[name] = {}
-
-                        if(!para[name].hasOwnProperty(pkey))
-                            para[name][pkey] = {}
-
-                        para[name][pkey][key] = value
-
-                    }else{
-                        if(!para.hasOwnProperty(name))
-                            para[name] = {}
-                        
-                        para[name][key] = value
-                    }
-                    return
-                }
-
-                para[name] = value
-                
-            })
-
-            form.on('file', (name, file) => {
-                if(name.slice(-2) === '[]'){
-                    name = name.slice(0, -2)
-                }
-                if(!para.hasOwnProperty(name)){
-                    para[name] = file
-                }else{
-                    if(!Array.isArray(para[name])){
-                        para[name] = [para[name]]
-                    }
-                    para[name].push(file)
-                }
-            })
-
-            form.on('error', no)
-
-            form.on('end', err => {
-                for(let i in para){
-                    if(typeof para[i] === 'string'){
-                        if(!para[i]){
-                            continue
-                        }
-                        if(para[i].startsWith(0) && !para[i].startsWith('0.'))
-                            continue
-                        else if(isFinite(para[i]))
-                            para[i] = +para[i]
-                    }
-                }
-                this._postParam = para
-                if(key)
-                    return yes(para.hasOwnProperty(key) ? para[key] : null)
-                else
-                    return yes(para)
-            })
-        })
-
-    }
-
-    //获取响应头
-    headers(key){
-        key = (key + '').toLowerCase()
-        return !!key ? this.req.headers[key] : this.req.headers
-    }
-
-    //获取客户端IP
-    ip(){
-        return this.headers('x-real-ip') || this.headers('x-forwarded-for') || this.req.connection.remoteAddress.replace('::ffff:', '')
-    }
-
-
-
-
+function defer() {
+  let obj = {}
+  obj.promise = new Promise((resolve, reject) => {
+    obj.resolve = resolve
+    obj.reject = reject
+  })
+  return obj
 }
 
+function hideProperty(host, name, value) {
+  Object.defineProperty(host, name, {
+    value: value,
+    writable: true,
+    enumerable: false,
+    configurable: true
+  })
+}
 
+class Request {
+  constructor(req, res) {
+    this.method = req.method.toUpperCase()
+    this.router = {}
+
+    hideProperty(this, 'origin', { req, res })
+    hideProperty(this, '__GET__', null)
+    hideProperty(this, '__POST__', null)
+
+    this.__fixUrl()
+
+    if (!FS.isdir(tmpdir)) {
+      FS.mkdir(tmpdir)
+    } else {
+      // 清除2个小时前的所有临时文件
+      let list = FS.ls(tmpdir)
+      list.forEach(it => {
+        if (FS.stat(tmpdir + it).atime < Date.now() - 2 * 3600 * 1000) {
+          FS.rm(tmpdir + it)
+        }
+      })
+    }
+  }
+
+  // 修正请求的url
+  __fixUrl() {
+    let _url = URL.parse(this.origin.req.url)
+      .pathname.slice(1)
+      .replace(/[\/]+$/, '')
+    let app = '' // 将作为主控制器(即apps目录下的应用)
+    let pathArr = []
+    let tmpArr = []
+
+    // URL上不允许有非法字符
+    if (/[^\w\-\/\.]/.test(_url)) {
+      this.origin.res.rendered = true
+      this.origin.res.writeHead(400, {
+        'X-debug': 'url[' + _url + '] contains illegal characters'
+      })
+      return this.origin.res.end('')
+    }
+
+    // 修正url中可能出现的"多斜杠"
+    _url = _url.replace(/[\/]+/g, '/').replace(/^\//, '')
+
+    pathArr = _url.split('/')
+    if (!pathArr[0] || pathArr[0] === '') {
+      pathArr[0] = 'index'
+    }
+
+    if (pathArr[0].indexOf('.') !== -1) {
+      app = pathArr[0].slice(0, pathArr[0].indexOf('.'))
+      // 如果app为空(这种情况一般是url前面带了个"."造成的),则自动默认为index
+      if (!app || app === '') {
+        app = 'index'
+      }
+    } else {
+      app = pathArr[0]
+    }
+
+    pathArr.shift()
+
+    // 将path第3段之后的部分, 每2个一组转为key-val数据对象, 存入router中
+    tmpArr = pathArr.slice(1).concat()
+    while (tmpArr.length) {
+      this.router[tmpArr.shift()] = tmpArr.shift() || null
+    }
+    tmpArr = undefined
+
+    for (let i in this.router) {
+      if (!this.router[i]) {
+        continue
+      }
+      // 修正数字类型,把符合条件的数字字符串转为数字(也许会误转, 但总的来说是利大弊)
+      if (this.router[i].startsWith(0) && !this.router[i].startsWith('0.')) {
+        continue
+      } else if (isFinite(this.router[i])) {
+        this.router[i] = +this.router[i]
+      }
+    }
+
+    this.app = app
+    this.url = _url
+    this.path = pathArr
+  }
+
+  /**
+   * [get 同php的$_GET]
+   */
+  get(key = '', xss = true) {
+    xss = !!xss
+    if (!this.__GET__) {
+      let para = URL.parse(this.origin.req.url).query
+      para = Object.assign({}, QS.parse(para))
+      if (xss) {
+        for (let i in para) {
+          if (!para[i]) {
+            continue
+          }
+
+          if (Array.isArray(para[i])) {
+            para[i] = para[i].map(it => {
+              it = it.trim().xss()
+              if (it.startsWith(0) && !it.startsWith('0.')) {
+                return it
+              } else if (isFinite(it)) {
+                it = +it
+              }
+              return it
+            })
+          } else {
+            para[i] = para[i].trim().xss()
+
+            if (para[i].startsWith(0) && !para[i].startsWith('0.')) {
+              continue
+            } else if (isFinite(para[i])) {
+              para[i] = +para[i]
+            }
+          }
+        }
+      }
+      this.__GET__ = para
+    }
+
+    return key
+      ? this.__GET__.hasOwnProperty(key) ? this.__GET__[key] : null
+      : this.__GET__
+  }
+
+  /**
+   * [post 接收post, 需要 await ]
+   * @param  {Str}    key      [字段]
+   */
+  post(key = '', xss = true) {
+    let para = {}
+    let out = defer()
+    xss = !!xss
+
+    //如果之前已经缓存过,则直接从缓存读取
+    if (this.__POST__) {
+      if (key) {
+        return this.__POST__.hasOwnProperty(key) ? this.__POST__[key] : null
+      } else {
+        return this.__POST__
+      }
+    }
+
+    let form = new Parser()
+    form.uploadDir = tmpdir
+    form.parse(this.origin.req)
+
+    form.on('field', (name, value) => {
+      if (/urlencoded/i.test(this.header('content-type'))) {
+        if (
+          name.slice(0, 2) === '{"' &&
+          (name.slice(-2) === '"}' || value.slice(-2) === '"}')
+        ) {
+          name = name.replace(/\s/g, '+')
+
+          if (value.slice(0, 1) === '=') value = '=' + value
+
+          return Object.assign(para, JSON.parse(name + value))
+        }
+      }
+
+      if (typeof value === 'string') {
+        value = xss ? value.xss() : value
+      }
+
+      if (name.slice(-2) === '[]') {
+        name = name.slice(0, -2)
+        if (typeof value === 'string') {
+          value = [value]
+        }
+      } else if (name.slice(-1) === ']') {
+        let key = name.slice(name.lastIndexOf('[') + 1, -1)
+        name = name.slice(0, name.lastIndexOf('['))
+
+        //多解析一层对象(也仅支持到这一层)
+        if (name.slice(-1) === ']') {
+          let pkey = name.slice(name.lastIndexOf('[') + 1, -1)
+          name = name.slice(0, name.lastIndexOf('['))
+
+          if (!para.hasOwnProperty(name)) {
+            para[name] = {}
+          }
+
+          if (!para[name].hasOwnProperty(pkey)) {
+            para[name][pkey] = {}
+          }
+
+          para[name][pkey][key] = value
+        } else {
+          if (!para.hasOwnProperty(name)) {
+            para[name] = {}
+          }
+
+          para[name][key] = value
+        }
+        return
+      }
+
+      para[name] = value
+    })
+
+    form.on('file', (name, file) => {
+      if (name.slice(-2) === '[]') {
+        name = name.slice(0, -2)
+      }
+      if (!para.hasOwnProperty(name)) {
+        para[name] = file
+      } else {
+        if (!Array.isArray(para[name])) {
+          para[name] = [para[name]]
+        }
+        para[name].push(file)
+      }
+    })
+
+    form.on('error', out.reject)
+
+    form.on('end', err => {
+      for (let i in para) {
+        if (typeof para[i] === 'string') {
+          if (!para[i]) {
+            continue
+          }
+          if (para[i].startsWith(0) && !para[i].startsWith('0.')) {
+            continue
+          } else if (isFinite(para[i])) {
+            para[i] = +para[i]
+          }
+        }
+      }
+      this._postParam = para
+      if (key) {
+        return out.resolve(para.hasOwnProperty(key) ? para[key] : null)
+      } else {
+        return out.resolve(para)
+      }
+    })
+    return out.promise
+  }
+
+  //获取响应头
+  header(key) {
+    key = (key + '').toLowerCase()
+    return !!key ? this.origin.req.headers[key] : this.origin.req.headers
+  }
+
+  //获取客户端IP
+  ip() {
+    return (
+      this.header('x-real-ip') ||
+      this.header('x-forwarded-for') ||
+      this.origin.req.connection.remoteAddress.replace('::ffff:', '')
+    )
+  }
+}
 
 module.exports = Request
